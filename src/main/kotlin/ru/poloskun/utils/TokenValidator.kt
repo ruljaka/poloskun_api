a385c8ea53339ac5ad1557fd64d3bd87aba71ae1
ruljaka <ruljaka@gmail.com>
2022-08-10 22:57:45 +0300

    CreateProductRequest

--- src/main/kotlin/ru/poloskun/utils/TokenValidator.kt
+++ src/main/kotlin/ru/poloskun/utils/TokenValidator.kt
@@ -5,5 +5,5 @@ import ru.poloskun.database.tokens.Tokens
 object TokenValidator {
     fun isTokenValid(token: String): Boolean = Tokens.fetchTokens().firstOrNull { it.token == token } != null
 
-    fun isTokenAdmin(token: String): Boolean = token == "bf8487ae-7d47-11ec-90d6-0242ac120003"
+    fun isTokenAdmin(token: String): Boolean = token == "d35ca962-7a23-4104-b1ce-2343f6f96093"
 }
