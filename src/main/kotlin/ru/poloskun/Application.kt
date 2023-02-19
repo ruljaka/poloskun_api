e999847d73175ae5b3c7abfdaa977294a4406ac6
r.grigorev <r.grigorev@astondevs.ru>
2023-02-19 17:20:16 +0300

    Heroku DB

--- src/main/kotlin/ru/poloskun/Application.kt
+++ src/main/kotlin/ru/poloskun/Application.kt
@@ -2,8 +2,8 @@ package ru.poloskun
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import org.jetbrains.exposed.sql.Database
 import ru.poloskun.features.login.configureLoginRouting
 import ru.poloskun.features.products.configureProductsRouting
@@ -12,14 +12,9 @@ import ru.poloskun.plugins.configureRouting
 import ru.poloskun.plugins.configureSerialization
 
 fun main() {
-//    Database.connect(url = "jdbc:postgresql://localhost:5432/poloskun", driver = "org.postgresql.Driver",
-//    user = "postgres", password = "pgLexicon1984")
-
     val config = HikariConfig("hikari.properties")
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
-
-    //embeddedServer(Netty, port = 8080) {
     embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         configureRouting()
         configureSerialization()
