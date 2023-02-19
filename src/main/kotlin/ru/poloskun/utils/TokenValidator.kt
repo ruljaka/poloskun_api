a7c5055b3c2c6fb24ab845c4ae1797ec230be7ea
r.grigorev <r.grigorev@astondevs.ru>
2023-02-19 17:13:21 +0300

    delete DB

--- src/main/kotlin/ru/poloskun/utils/TokenValidator.kt
+++ src/main/kotlin/ru/poloskun/utils/TokenValidator.kt
@@ -5,5 +5,6 @@ import ru.poloskun.database.tokens.Tokens
 object TokenValidator {
     fun isTokenValid(token: String): Boolean = Tokens.fetchTokens().firstOrNull { it.token == token } != null
 
-    fun isTokenAdmin(token: String): Boolean = token == "d35ca962-7a23-4104-b1ce-2343f6f96093"
+    //fun isTokenAdmin(token: String): Boolean = token == "d35ca962-7a23-4104-b1ce-2343f6f96093"
+    fun isTokenAdmin(token: String): Boolean = token == "d00f5f04-78b2-48af-866a-977ae1a4d1ee"
 }
